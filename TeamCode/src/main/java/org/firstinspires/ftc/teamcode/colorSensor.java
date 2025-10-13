d62f69f5b51952fe175054355c1cddbe55e19efe
Niranjan <niranjan.anandakrishnan@gmail.com>
2025-10-13 17:01:56 -0500

    10/13 17:00- Color Sensor bugfix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/colorSensor.java
@@ -1,71 +1,43 @@
-
 package org.firstinspires.ftc.teamcode;
-
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 import com.qualcomm.robotcore.hardware.NormalizedRGBA;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
-@TeleOp(name = "Color Sensor Test"/*, group = "goo" */)
+import org.firstinspires.ftc.robotcore.external.JavaUtil;
+@TeleOp(name = "Color Sensor Test" , group = "Color")
 public class colorSensor extends OpMode {
     public NormalizedColorSensor colorSensor;
-
-    boolean detectingPurple;
-
-
 
     @Override
     public void init() {
         colorSensor = hardwareMap.get(NormalizedColorSensor.class, "revColorV3");
         colorSensor.setGain(7);
-        detectingPurple = false;
+
     }
 
     public void loop() {
-        //return 4 values, red, green, blue, alpha(brightness)
+
         NormalizedRGBA colors = colorSensor.getNormalizedColors();
 
-        float normRed, normGreen, normBlue, normPurple;
-        normRed = colors.red / colors.alpha;
-        normGreen = colors.green / colors.alpha;
-        normBlue = colors.blue / colors.alpha;
-        normPurple = (colors.blue + colors.green) / colors.alpha;
+        int col = colors.toColor();
+        double hue = JavaUtil.colorToHue(col);
 
+        telemetry.addData("Detected Hue", hue);
+        telemetry.addData("Detected Color", col);
+
+        if (hue > 15 && hue < 60) {
+            telemetry.addLine("Detected Color: Red");
+        } else if (hue > 90 && hue < 180) {
+            telemetry.addLine("Detected Color: Green");
+        } else if (hue > 200 && hue < 210) {
+            telemetry.addLine("Detected Color: Blue");
+        } else if (hue > 225 && hue < 350) {
+            telemetry.addLine("Detected Color: Purple");
+        }
 
         telemetry.setMsTransmissionInterval(25);
-
-        telemetry.addData("red", normRed);
-        telemetry.addData("green", normGreen);
-        telemetry.addData("blue", normBlue);
-        telemetry.addData("purple", normPurple);
-
-        telemetry.addData("Alpha", colors.alpha);
-
-        telemetry.addLine("Detecting-----------------------------------------------------");
-
-        if (((normGreen + normBlue)/ 1.5) < normRed) {
-            telemetry.addLine("Red");
-            detectingPurple = false;
-        }
-
-
-        if (((normRed + normBlue)/ 1.3) < normGreen) {
-            telemetry.addLine("Green");
-            detectingPurple = false;
-        }
-
-        if ((((normRed + normGreen) / 1.5) < normBlue) && !detectingPurple) {
-            telemetry.addLine("Blue");
-            detectingPurple = false;
-        }
-
-        if (((((normRed + normBlue) / 1.2 + normGreen))/ 1.5) < normBlue) {
-            telemetry.addLine("Purple");
-            detectingPurple = true;
-        }
 
         telemetry.update();
 
