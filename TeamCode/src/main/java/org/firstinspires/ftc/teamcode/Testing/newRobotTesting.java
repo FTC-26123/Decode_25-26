3ea876d3b82ee989f699156055494c392649014b
gahutgahut <neelsai2012@gmail.com>
2026-01-18 19:41:38 -0600

    new robot testing file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/newRobotTesting.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "newRobotTesting")
+public class newRobotTesting extends OpMode {
+
+    public DcMotor frontLeftMotor;
+    public DcMotor frontRightMotor;
+    public DcMotor backLeftMotor;
+    public DcMotor backRightMotor;
+    public DcMotorEx launcherRight;
+
+    public DcMotor index;
+    public DcMotor intake;
+
+    public DcMotorEx launcherLeft;
+    @Override
+    public void init() {
+        frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+        launcherRight = hardwareMap.get(DcMotorEx.class, "launcher");
+        index = hardwareMap.get(DcMotor.class, "windmill");
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        launcherLeft = hardwareMap.get(DcMotorEx.class, "launcherLeft");
+
+        launcherRight.setDirection(DcMotorSimple.Direction.REVERSE);;
+        index.setDirection(DcMotorSimple.Direction.REVERSE);
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+    @Override
+    public void loop() {
+
+        telemetry.addLine("Gamepad2.a -> intake");
+        if (gamepad2.a) {
+            intake.setPower(1);
+        }
+
+        telemetry.addLine("Gamepad2.b -> index / windmill");
+        if (gamepad2.b) {
+            index.setPower(1);
+        }
+
+        telemetry.addLine("Gamepad2.y -> left launcher");
+        telemetry.addData("Launch Power (left)", launcherLeft.getVelocity());
+        if (gamepad2.y) {
+            launcherLeft.setVelocity(2250);
+        }
+
+        telemetry.addLine("Gamepad2.x -> right launcher");
+        telemetry.addData("Launch Power (right)", launcherRight.getVelocity());
+        if (gamepad2.x) {
+            launcherRight.setVelocity(2250);
+        }
+
+        telemetry.addLine("Gamepad2.ps -> all off");
+        if (gamepad2.ps) {
+            intake.setPower(0);
+            launcherLeft.setVelocity(0);
+            launcherRight.setVelocity(0);
+            index.setPower(0);
+        }
+
+        telemetry.addLine("Gamepad2.start -> reverse motor");
+        if (gamepad2.start) {
+            launcherRight.setDirection(DcMotorSimple.Direction.FORWARD);;
+            launcherLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+            index.setDirection(DcMotorSimple.Direction.FORWARD);
+            intake.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+
+        telemetry.addLine("Gamepad2.back -> forward motor");
+        if (gamepad2.back) {
+            launcherRight.setDirection(DcMotorSimple.Direction.REVERSE);;
+            launcherLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+            index.setDirection(DcMotorSimple.Direction.REVERSE);
+            intake.setDirection(DcMotorSimple.Direction.REVERSE);
+        }
+
+        telemetry.update();
+
+    }
+
+
+}
